c7bfb5aa57dfd5067312c7206e2e03d8a15350c6
KcLivings <clivings@uccs.edu>
2023-03-22 15:49:42 +0000

    finished GEO6 and added all tests

--- spec/features/projects_feature_spec.rb
+++ spec/features/projects_feature_spec.rb
@@ -7,7 +7,6 @@ RSpec.feature "Projects", type: :feature do
         visit edit_project_path(project)
       end
  
- 
       scenario "should be successful" do
         within("form") do
           fill_in "Description", with: "New description content"
@@ -16,8 +15,7 @@ RSpec.feature "Projects", type: :feature do
         expect(page).to have_content("Project was successfully updated")
       end
  
- 
-      scenario "descrypt should fail" do
+      scenario "descript should fail" do
         within("form") do
           fill_in "Description", with: ""
         end
@@ -27,10 +25,43 @@ RSpec.feature "Projects", type: :feature do
       
       scenario "title should fail" do
         within("form") do
-          fill_in "title", with: ""
+          fill_in "Title", with: ""
         end
         click_button "Update Project"
         expect(page).to have_content("Title can't be blank")
+      end
+    end
+
+    context "create project" do
+      before(:each) do
+        visit new_project_path()#edit_project_path(project)
+      end
+
+      scenario "should be successful" do
+        within("form") do
+          fill_in "Title", with: "title stuff"
+          fill_in "Description", with: "description content"
+        end
+        click_button "Create Project"
+        expect(page).to have_content("Project was successfully created")
+      end
+
+      scenario "title should fail" do
+        within("form") do
+          fill_in "Title", with: ""
+          fill_in "Description", with: "descript Stuff"
+        end
+        click_button "Create Project"
+        expect(page).to have_content("Title can't be blank")
+      end
+
+      scenario "Descript should fail" do
+        within("form") do
+          fill_in "Title", with: "title stuff"
+          fill_in "Description", with: ""
+        end
+        click_button "Create Project"
+        expect(page).to have_content("Description can't be blank")
       end
     end
 end
